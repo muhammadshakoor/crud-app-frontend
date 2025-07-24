e805d27d8a8f2ef0a53297ba9495ab36c60143fd
Muhammad Shakoor <muhammadshakoor86@gmail.com>
2025-07-24 18:06:03 +0500

    create .env

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+
 const api = axios.create({
   // baseURL: 'http://localhost:8080/api',
-  baseURL: 'crud-app-backend-sepia.vercel.app/api',
+  baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: 'https://crud-app-backend-sepia.vercel.app/api',
 });
 
 api.interceptors.request.use(config => {
