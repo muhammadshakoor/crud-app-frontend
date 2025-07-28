e6e1b9517474901cbf022d9f00f0fb29a447292b
Muhammad Shakoor <muhammadshakoor86@gmail.com>
2025-07-28 15:21:55 +0500

    add N/A to min-stock-level in the table

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -385,7 +385,7 @@ const ProductList: React.FC = () => {
                   <td className="px-4 py-2">{product.sale_price ? `$${Number(product.sale_price).toFixed(2)}` : 'N/A'}</td>
                   <td className="px-4 py-2">{product.tax_rate ? `${product.tax_rate}%` : 'N/A'}</td>
                   <td className="px-4 py-2">{product.track_inventory ? 'Yes' : 'No'}</td>
-                  <td className="px-4 py-2">{product.min_stock_level}</td>
+                  <td className="px-4 py-2">{product.min_stock_level || 'N/A'}</td>
                   <td className="px-4 py-5 flex gap-2 justify-center items-center">
                     <button onClick={() => setViewingProduct(product)} className="text-blue-600 border border-blue-600 hover:bg-blue-600 hover:text-white px-2 py-1 text-sm rounded transition duration-200">👁️ {/*View*/}</button>
                     <button onClick={() => setEditingProduct(product)} className="text-green-600 border border-green-600 hover:bg-green-600 hover:text-white px-2 py-1 text-sm rounded transition duration-200">✏️ {/*Edit*/}</button>
